cd1df18876f9b0c5ca7cb278af40324314ba73f7
Kirill <kirweekend@mail.ru>
2016-11-27 16:45:40 +0200

    motion along X coordinate

--- core/src/com/packt/square/GameScreen.java
+++ core/src/com/packt/square/GameScreen.java
@@ -14,6 +14,10 @@ import java.awt.*;
 public class GameScreen extends ScreenAdapter {
     private SpriteBatch batch;
     private Texture squareMain;
+    private static final float MOVE_TIME = 0.1F;
+    private static final int SQUARE_MOVEMENT = 32;
+    private float timer = MOVE_TIME;
+    private int squareX = 0, squareY = 0;
 
     @Override
     public void show() {
@@ -21,12 +25,24 @@ public class GameScreen extends ScreenAdapter {
         batch = new SpriteBatch();
     }
 
+    private void checkForBounds() {
+        if (squareX >= Gdx.graphics.getWidth()) {
+            squareX = 0;
+        }
+    }
+
     @Override
     public void render(float delta) {
+        timer -= delta;
+        if (timer <= 0) {
+            timer = MOVE_TIME;
+            squareX += SQUARE_MOVEMENT;
+        }
+        checkForBounds();
         Gdx.gl.glClearColor(Color.BLACK.getRed(), Color.BLACK.getGreen(), Color.BLACK.getBlue(), Color.BLACK.getAlpha());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(squareMain, 0, 0);
+        batch.draw(squareMain, squareX, squareY);
         batch.end();
     }
 }
