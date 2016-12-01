a68c49e92fb8f41f8288e88f5209e7453f41e1d9
Kirill <kirweekend@mail.ru>
2016-12-01 19:04:54 +0200

    wrote resolution

--- desktop/src/com/packt/square/desktop/DesktopLauncher.java
+++ desktop/src/com/packt/square/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.packt.square.SquareGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 640;
+        config.height = 480;
         new LwjglApplication(new SquareGame(), config);
     }
 }
